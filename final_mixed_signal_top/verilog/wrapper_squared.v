7ceb83317fc85df9cef1df217a22e217230c44ae
ellen-wood <121111366+ellen-wood@users.noreply.github.com>
2022-12-31 12:41:32 +0000

    minimal verilog to enable wiring - for now

---
+++ final_mixed_signal_top/verilog/wrapper_squared.v
@@ -0,0 +1,15 @@
+module wrapper_squared
+(
+  output wire [37:0] io_out,
+  output wire [37:0] io_oeb,
+  inout wire vccd1,
+  inout wire vcca1,
+  inout wire vssa1,
+  inout wire vssd1,
+  inout wire [10:0] analog_io,
+  input wire wb_clk_i,
+  input wire [37:0] io_in,
+  input wire active
+);
+
+endmodule
